654b61705a39ce5d69c2c6943fa98d5c2a5ecfb8
zakiaazizi-17 <z.azizi2447@uca.ac.ma>
2026-03-09 00:37:46 +0000

    tp1react

--- src/App.js
+++ src/App.js
@@ -21,5 +21,16 @@ function App() {
     </div>
   );
 }
+// App.js
+import HelloWorld from './HelloWorld';
 
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
 export default App;
---
+++ src/HelloWorld.js
@@ -0,0 +1,6 @@
+// HelloWorld.js
+function HelloWorld() {
+  return <h1>Hello guys !</h1>;
+}
+
+export default HelloWorld;
